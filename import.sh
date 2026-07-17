ab7139f301da6d89a29dff84853524a16aa6a840
Martín López Mireles <martinh.mireles@gmail.com>
2026-07-17 10:53:16 -0600

    Add import script
    Signed-off-by: Martín López Mireles <martinh.mireles@gmail.com>

---
+++ import.sh
@@ -0,0 +1,5 @@
+cp ~/.dotfiles/.tmux.conf ~/.tmux.conf
+cp ~/.dotfiles/.zshrc ~/.zshrc
+cp ~/.dotfiles/hs.zsh ~/.oh-my-zsh/custom
+cp ~/.dotfiles/hs.zsh-theme ~/.oh-my-zsh/custom/themes
+cp ~/.dotfiles/my_configs.vim ~/.vim_runtime
